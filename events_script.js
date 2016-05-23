4f1c624777469a918a7fbcdf8d16bc24f725c066
Thwipshot <ethumpert@gmail.com>
2016-05-23 15:17:30 -0500

    passed 6 of 9 test cases for the backbone events testing but I haven't passed test 6 yet.

--- events_script.js
+++ events_script.js
@@ -5,7 +5,29 @@ function Animal(type) {
 }
 
 function Circus() {
+  this.animals = [];
+}
+
+Circus.prototype.add = function(animal) {
+  this.animals.push(animal);
+}
+
+Circus.prototype.animalCount = function() {
+  return this.animals.length;
 }
 
 function Ringmaster() {
+  this.isCalm = true;
+}
+
+_.extend(Circus.prototype, Backbone.Events);
+
+Ringmaster.prototype.watch = function (watchingShow) {
+  var self = this;
+  watchingShow.on ('Show Time!', function() {
+    if (Animal.type === 'lion') {
+      self.isCalm == false;
+    }
+  })
+
 }
