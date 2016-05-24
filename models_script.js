4fe463e6ea90bfcc9a806f6b253ef1315e6872f1
Thwipshot <ethumpert@gmail.com>
2016-05-24 15:52:26 -0500

    passed all 8 test cases

--- models_script.js
+++ models_script.js
@@ -1,7 +1,33 @@
 'use strict';
 
 var FurnitureStore = Backbone.Model.extend({
+
+  defaults: {
+    brokenFurnitureCount: 0,
+    furnitureCountToBeExamined: 0,
+  },
+
+  track: function(furniture) {
+    var self = this;
+      self.listenTo(furniture, 'smash', function(furniture) {
+        var count = self.get('brokenFurnitureCount');
+        self.set('brokenFurnitureCount', count += 1);
+    });
+    furniture.on("change:condition", function() {
+      var count = self.get("furnitureCountToBeExamined");
+      self.set("furnitureCountToBeExamined", count += 1);
+    })
+  }
 });
 
 var Furniture = Backbone.Model.extend({
+  defaults: {
+    condition: 'new',
+    material: 'oak',
+    price: 0,
+  },
+  smash: function(furniture) {
+    this.trigger('smash');
+  }
+
 });
